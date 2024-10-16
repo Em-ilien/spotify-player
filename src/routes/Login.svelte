75a49c27d7bade954383066f3cff5e916e0aa70c
Emilien Cosson <emilien@em-ilien.fr>
2024-10-16 02:42:06 +0200

    refactor: Update Login.svelte to include welcome message
    Update Login.svelte to include a welcome message that provides an overview of the Spotify Player app and instructions for logging in and accessing personalized music library.

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -3,11 +3,22 @@
 	import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 </script>
 
-<div class="flex justify-center items-center h-screen">
+<div class="flex flex-col justify-center items-center h-screen">
 	<button
 		class="bg-black text-white font-bold py-2 px-4 rounded hover:bg-black transition duration-300"
 		on:click={() => (window.location.href = '/login')}
 	>
-		<FontAwesomeIcon icon={faSignInAlt} />
+		<FontAwesomeIcon icon={faSignInAlt} class="mr-2" /> <span>Login</span>
 	</button>
+
+	<div class="mt-8">
+		<p class="color-gray-500">
+			Welcome to Spotify Player! This app allows you to log in with your Spotify account and view
+			your favorite tracks, artists, and playlists.
+		</p>
+		<p class="color-gray-500">
+			Click the login button above to get started and explore your personalized music library.
+		</p>
+		<p class="color-gray-500">You must have the Spotify app open on your device.</p>
+	</div>
 </div>
