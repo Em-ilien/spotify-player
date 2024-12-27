c74bbad9cec00a5c9e33100740f22be00e0b8d66
Emilien Cosson <emilien@em-ilien.fr>
2024-12-27 21:07:10 +0100

    fix: add login page infos pagrapraphs

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -12,6 +12,9 @@
 	</button>
 
 	<div class="mt-8">
+		<p class="color-gray-500 font-bold mb-4">
+			Currently the app is in private beta, come back later!
+		</p>
 		<p class="color-gray-500">
 			Welcome to Spotify Player! This app allows you to log in with your Spotify account and view
 			your favorite tracks, artists, and playlists.
@@ -20,5 +23,6 @@
 			Click the login button above to get started and explore your personalized music library.
 		</p>
 		<p class="color-gray-500">You must have the Spotify app open on your device.</p>
+		<p class="color-gray-500">Your account must have the Spotify Premium subscription.</p>
 	</div>
 </div>
